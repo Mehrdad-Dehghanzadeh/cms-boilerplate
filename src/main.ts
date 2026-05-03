e2d091348a90743bb443f4b635919935242566f3
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2026-05-04 01:18:06 +0330

    feat: add prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import process from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`app is ready on ${process.env.PORT}`);
 }
 bootstrap();
