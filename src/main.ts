305253f3253af523b941586b6ee00f145739e119
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2026-05-09 00:15:55 +0330

    feat: remove scheama prisma

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module.js';
 import process from 'node:process';
+import { PrismaPg } from '@prisma/adapter-pg';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   await app.listen(process.env.PORT ?? 3000);
   console.log(`app is ready on ${process.env.PORT}`);
 }
