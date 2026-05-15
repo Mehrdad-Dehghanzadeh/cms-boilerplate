6aa2919476990799c49efbbf0cdf9a4f6897ae19
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2026-05-15 20:56:45 +0330

    database-query

---
+++ database-query.sql
@@ -0,0 +1,13 @@
+-- ساخت کاربر
+CREATE ROLE cms_user
+WITH LOGIN
+PASSWORD '1';
+
+-- ساخت دیتابیس با collation فارسی و owner
+CREATE DATABASE cms_database
+WITH
+    OWNER = cms_user
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'fa_IR.UTF-8'
+    LC_CTYPE = 'fa_IR.UTF-8'
+    TEMPLATE = template0;
