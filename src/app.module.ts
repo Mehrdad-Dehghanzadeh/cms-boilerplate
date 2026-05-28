e4f48587991a743c3e6419d5adf0f7ba6c934272
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2026-05-28 12:28:21 +0330

    config: update prettierrc

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,8 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { GatewayModule } from '@modules/gateway.module';
-import { DatabaseModule, LoggingModule } from '@infrastructure';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { GatewayModule } from '@modules/gateway.module'
+import { DatabaseModule, LoggingModule } from '@infrastructure'
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    LoggingModule,
-    GatewayModule,
-    DatabaseModule,
-  ],
+  imports: [ConfigModule.forRoot(), LoggingModule, GatewayModule, DatabaseModule]
 })
 export class AppModule {}
